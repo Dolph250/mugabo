faeee28352265ddb3e980f24810e89b15d9e4825
LS2425229 <ls2425229@example.com>
2025-04-16 15:40:20 +0800

    Added homepage and about page

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import Particle from "../Particle";
+
+
+
+
+
+
+function ResumeNew() {
+
+
+
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+     
+
+     
+
+   
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
