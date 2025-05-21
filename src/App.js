03671ac22c373373cae6abbabb012505b8760230
LS2425229 <ls2425229@example.com>
2025-05-22 03:08:17 +0800

    Rectified issue on appJS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
@@ -8,12 +9,6 @@ import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import Assignment from "./components/Projects/Assignment";
 import Assignmenter from "./components/Projects/Assignmenter";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
@@ -26,12 +21,11 @@ function App() {
     const timer = setTimeout(() => {
       upadateLoad(false);
     }, 1200);
-
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <Router>
+    <BrowserRouter basename="/mugabo">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
@@ -42,12 +36,12 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/Assignment" element={<Assignment />} />
-           <Route path="/Assignmenter" element={<Assignmenter />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="/Assignmenter" element={<Assignmenter />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
